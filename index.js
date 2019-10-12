2a62a38004510cc47f75e63651e7b60c6ef08627
adoniramj <adoniramjvargas@gmail.com>
2019-10-12 15:26:37 -0400

    questions.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 let readlineSync = require('readline-sync');
+let nl = () => { console.log('\n') }
+let clear = () => { process.stdout.write('\033c') }
+
+clear()
+let score;
+let input;
+//readlineSync.question("Options: yes or no? ");
+let userName = readlineSync.question('Hi. Who am I communicating with? ');
+nl();
+console.log(`Hi ${userName}. I am the HAL-9000 interstellar vacation planner.`)
+nl();
+console.log(`${userName}, I am going to ask you a set of questions.`);
+nl();
+console.log(`If you get a perfect score, the trip will be to outer space.\nIf you get a score of zero your vacation will be in Dade County.\nFor any other score we will plan a trip here on Earth.`)
+
+
+
---
+++ questions.js
@@ -0,0 +1,11 @@
+let questions = [
+    {
+        question: 'This is a question',
+        options: {
+            a: 'first option',
+            b: 'second option',
+            c: 'third option'
+        },
+        answer: 'b'
+    }
+]
